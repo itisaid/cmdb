8e0a0ec0f8fc670e25726ff6007ecb8b163b99c3
Li Zhihui <zhihui.li@intel.com>
2013-06-28 11:45:37 +0800

    create project

---
+++ src/main/java/com/10bd/App.java
@@ -0,0 +1,13 @@
+package com.10bd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
