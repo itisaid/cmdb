7462ba4c5a4509ffda4667c0dae70c7c3ba91fe7
Li Zhihui <zhihui.li@intel.com>
2013-07-16 17:32:07 +0800

    subject detail

--- cmspider/src/main/java/com/hbd/cmdb/BaseInfo.java
+++ cmspider/src/main/java/com/hbd/cmdb/BaseInfo.java
@@ -2,7 +2,7 @@ package com.hbd.cmdb;
 
 public class BaseInfo {
 	public static String path = "/home/frank/work/github/cmdb/research/";
-	public static String indexPath = "C:\\work\\github\\cmdb\\research\\";
+	public static String indexPath = "/home/frank/work/github/cmdb/research/";
 	public static String output = "/home/frank/cmdb/";
 	public static String subjectPath = BaseInfo.path + "subject.txt";
 	public static String reviewFile = "/review.dat";
--- cmspider/src/main/java/com/hbd/cmdb/Subject.java
+++ cmspider/src/main/java/com/hbd/cmdb/Subject.java
@@ -1,17 +1,88 @@
 package com.hbd.cmdb;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Subject {
 
 	private String title;
-	private String year;
-	private List<String> directors = new ArrayList<String>();
-	private List<String> writers = new ArrayList<String>();
-	private List<String> stars = new ArrayList<String>();
-	private List<String> genres = new ArrayList<String>();
+	private int year;
+	private String directors;
+	private String writers;
+	private String stars;
+	private String genres;
 	private String zero;
 	private String language;
-	private String releaseDate; 
+	private int releaseDate;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getDirectors() {
+		return directors;
+	}
+
+	public void setDirectors(String directors) {
+		this.directors = directors;
+	}
+
+	public String getWriters() {
+		return writers;
+	}
+
+	public void setWriters(String writers) {
+		this.writers = writers;
+	}
+
+	public String getStars() {
+		return stars;
+	}
+
+	public void setStars(String stars) {
+		this.stars = stars;
+	}
+
+	public String getGenres() {
+		return genres;
+	}
+
+	public void setGenres(String genres) {
+		this.genres = genres;
+	}
+
+	public String getZero() {
+		return zero;
+	}
+
+	public void setZero(String zero) {
+		this.zero = zero;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public int getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(int releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
 }
